eaddf6ad1b23c87dccafe4144a099b9f1b49af84
Gleb-Savchenkov <64221348+Gleb-Savchenkov@users.noreply.github.com>
2020-07-25 16:44:25 +0300

    Добавьте файлы проекта.

---
+++ Noughts and Crosses/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Noughts_and_Crosses
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void POOD_Click(object sender, RoutedEventArgs e)
+        {
+            // Кнопка Play on one device открывает окно игры с самим собой на одном устройстве
+            new Win_POOD().ShowDialog();
+        }
+    }
+}
